746f5d0de0d775b628ee7dcd7bfc194e6538fe97
Szabo Rihcard <ignocius@gmail.com>
2016-11-04 14:09:03 +0100

    day3 til-8 100%

--- week-03/day-3/09.py
+++ week-03/day-3/09.py
@@ -6,3 +6,14 @@
 #   *****
 #   ******
 # It should take a number as parameter that describes how many lines the triangle has
+
+def print_star(param):
+    star ="*"
+    x = 0
+    while x <= param:
+        print(star)
+        x += 1
+        for i in range(x):
+            print(star)
+
+print_star(3)
