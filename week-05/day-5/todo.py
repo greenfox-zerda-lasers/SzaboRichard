63bcaf3b88381c631cc9175ccd99fe2f7943bdb4
Szabo Rihcard <ignocius@gmail.com>
2016-11-18 10:46:18 +0100

    todo app usage -review

--- week-05/day-5/todo.py
+++ week-05/day-5/todo.py
@@ -4,10 +4,18 @@ from todo_view import TodoView
 class TodoCtrl:
     def __init__(self):
         self.view = TodoView()
+        self.input_list = ["todo.py"]
+        self.input_l_len = len(self.input_list)
+        self.sys_args_checker()
 
     def sys_args_checker(self):
-        print(sys.argv)
-        if len(sys.argv) == 1:
-            print("No argument was given")
-        elif len(sys.argv) > 1:
+        print(self.input_list)
+        if self.input_l_len == 1:
             print(self.view.usage_print['usage'])
+
+
+    def file_open_read(self, inputt):
+        f = open(inputt)
+        data = f.readlines()
+
+ctrl = TodoCtrl()
