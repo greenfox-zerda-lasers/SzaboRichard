c9d7485b57e6944ae85fb86a976cb38b532f702e
Szabo Rihcard <ignocius@gmail.com>
2016-12-03 12:38:44 +0100

    new2 working as intended, tested

--- week-07-home-alone/gyak/examnew2.py
+++ week-07-home-alone/gyak/examnew2.py
@@ -1,3 +1,19 @@
 # Create a function that takes a filename as string parameter,
 # and counts the occurances of the letter "a", and returns it as a number.
 # It should not break if the file does not exist, just return 0.
+
+def a_counter_from_file(file_name):
+    try:
+        f = open(file_name, "r")
+        thefile = f.readlines()
+        a_count = 0
+        for lines in thefile:
+            for char in lines:
+                if char == "a":
+                    a_count+= 1
+        return a_count
+    except FileNotFoundError:
+        print(0)
+
+print(a_counter_from_file("tree.txt"))
+print(a_counter_from_file("testr.txt"))
