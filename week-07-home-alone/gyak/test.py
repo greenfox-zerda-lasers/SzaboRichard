60f1b0762d0cc1638f77e393789a45a75d6b5753
Szabo Rihcard <ignocius@gmail.com>
2016-12-03 12:57:14 +0100

    new4 working as intended, tested

--- week-07-home-alone/gyak/test.py
+++ week-07-home-alone/gyak/test.py
@@ -1,7 +1,8 @@
 import unittest
-from exam3  import count_letter_in_string
+from exam3 import count_letter_in_string
+from examnew4 import greeter
 
-class TestForExamThree(unittest.TestCase):
+class TestForExamThreeandFour(unittest.TestCase):
     def test_simple_word_and_letter_count(self):
         self.assertEqual(count_letter_in_string("alma", "a"), 2)
 
@@ -13,6 +14,20 @@ class TestForExamThree(unittest.TestCase):
 
     def test_string_list_for_input(self):
         self.assertEqual(count_letter_in_string(["stri","ng"], ["ng"]), 0)
+
+    # --------- Example 4 unittest
+
+    def test_greeter_with_empty_input(self):
+        self.assertEqual(greeter(''), 'Hello, Mr Nobody!')
+
+    def test_greeter_with_number_input(self):
+        self.assertEqual(greeter(3), 'Hello, 3!')
+
+    def test_greeter_with_name_input(self):
+        self.assertEqual(greeter('Bela'), 'Hello, Bela!')
+
+    def test_greeter_with_list_input(self):
+        self.assertRaises(TypeError, greeter(['string','sdad']))
 
 if __name__ == '__main__':
     unittest.main()
