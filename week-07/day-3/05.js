84898670e7c5d128983798577e92d7710ba88267
Szabo Rihcard <ignocius@gmail.com>
2016-12-07 11:32:39 +0100

    redesigned with filter + map, 5 complete

--- week-07/day-3/05.js
+++ week-07/day-3/05.js
@@ -7,13 +7,19 @@ var numbers = [2.4, 3.5, 1.7, 3.3, 1.2];
 // bigger than 2 and all of it's elements should be rounded
 
 function newReturnedArrayWithRoundedNum(arrayInput) {
-  var returnedArry = [];
-  arrayInput.forEach(function(number){
-    if (number > 2) {
-      returnedArry.push(Math.round(number));
-    }
-  });
+   var returnedArry = arrayInput.filter(function(num) {
+       return num > 2;
+   }).map(function(num) {
+       return Math.round(num);
+   });
+  // arrayInput.forEach(function(number){
+  //   if (number > 2) {
+  //     returnedArry.push(Math.round(number));
+  //   }
+  // });
+
   return returnedArry;
+
 }
 
 console.log(newReturnedArrayWithRoundedNum(numbers));
