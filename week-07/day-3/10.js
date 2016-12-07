1bffae920bdb8caa214cbc45ea9e37f57917454e
Szabo Rihcard <ignocius@gmail.com>
2016-12-07 14:36:48 +0100

    10 rdy +extra function

--- week-07/day-3/10.js
+++ week-07/day-3/10.js
@@ -1,29 +1,35 @@
 'use strict';
 
 // create a student object
-// that has a method `addgrade`, that takes a grade from 1 to 5
+// that has a method `addGrade`, that takes a grade from 1 to 5
 // an other method `getAverage`, that returns the average of the grades
 
 function Student(studGrade) {
   this.grade = studGrade;
-  this.grades = []
-  this.addgrade = function(grade) {
+  this.grades = [];
+  this.addGrade = function(grade) {
     if (grade >= 1 && grade <= 5) {
       this.grades.push(grade);
     }
-  }
+  };
+  this.addGrades = function(gradeArray) {
+    gradeArray.forEach(function(elem) {
+      this.addGrade(elem);
+    }, this);
+  };
   this.getAverage = function(studGrade) {
     var sum = 0;
     this.grades.forEach(function(elem) {
       sum += elem;
     });
     return sum / this.grades.length;
-   }
+  };
 }
 
 var stundent = new Student();
-stundent.addgrade(1);
-stundent.addgrade(5);
-stundent.addgrade(4);
-stundent.addgrade(2);
+stundent.addGrade(1);
+stundent.addGrades([1,4,3,1,1,4,1,5]);
+stundent.addGrade(5);
+stundent.addGrade(4);
+stundent.addGrade(2);
 console.log(stundent.getAverage());
