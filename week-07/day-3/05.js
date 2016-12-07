5f7a5a64c67c111ae03f799359ebcb047d1ebafa
Szabo Rihcard <ignocius@gmail.com>
2016-12-07 11:11:50 +0100

    5 complete

---
+++ week-07/day-3/05.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var numbers = [2.4, 3.5, 1.7, 3.3, 1.2];
+
+// create a function that takes an array of numbers,
+// it should return a new array that consists only the numbers that are
+// bigger than 2 and all of it's elements should be rounded
+
+function newReturnedArrayWithRoundedNum(arrayInput) {
+  var returnedArry = [];
+  arrayInput.forEach(function(number){
+    if (number > 2) {
+      returnedArry.push(Math.round(number));
+    }
+  });
+  return returnedArry;
+}
+
+console.log(newReturnedArrayWithRoundedNum(numbers));
