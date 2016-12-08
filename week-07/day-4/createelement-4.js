aec73003f6d22fb625c15da32cd88f0b2661724a
Szabo Rihcard <ignocius@gmail.com>
2016-12-08 16:59:37 +0100

    createelement all finished

--- week-07/day-4/createelement-4.js
+++ week-07/day-4/createelement-4.js
@@ -2,8 +2,11 @@ var myList = document.querySelector('.asteroids');
 var removedKing = myList.removeChild(document.querySelector('.asteroids li'));
 
 planetData.forEach(function(value){
-  if (value['asteroid']) {
-
+  if (value.asteroid) {
+     var newLi = document.createElement('li');
+     newLi.classList.add(value.category);
+     newLi.textContent = value.content;
+     myList.appendChild(newLi);
   }
   // console.log(value);
 });
