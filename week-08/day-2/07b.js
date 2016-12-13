0b5def4dad0914ef2740d602b73c876f1a84fd29
Szabo Rihcard <ignocius@gmail.com>
2016-12-13 15:06:08 +0100

    nasty evil black magic 7c rdy

--- week-08/day-2/07b.js
+++ week-08/day-2/07b.js
@@ -3,10 +3,10 @@
 //  - Create an image tag in the with document.createElement
 //  - Add a 'load' event to the image and only show the image to the user when the image is loaded
 
-var img = document.createElement('img');
-var hiddenOrNot = function(){
-  img.style.visibility = 'visible';
-};
+  var img = document.createElement('img');
+  var hiddenOrNot = function(){
+    img.style.visibility = 'visible';
+  };
 img.setAttribute('src', 'https://c7.staticflickr.com/8/7562/15584243094_b268a32192_h.jpg');
 img.style.visibility = 'hidden';
 img.addEventListener('load', hiddenOrNot);
---
+++ week-08/day-2/07c.js
@@ -0,0 +1,26 @@
+// Based on the previous example create an array of images taken from flickr
+// https://www.flickr.com/photos/jasontravis/26683911430/in/album-72157603258446753/
+// https://www.flickr.com/photos/jasontravis/16635664865/in/album-72157603258446753/
+// https://www.flickr.com/photos/jasontravis/14195441260/in/album-72157603258446753/
+
+// Display a progress bar while the images are loading
+// You can create your own or use the built in HTML5 version:
+// https://developer.mozilla.org/en/docs/Web/HTML/Element/progress
+
+var myImgFolder = ['https://c7.staticflickr.com/8/7799/26683911430_c4662bf0ec_z.jpg', 'https://c2.staticflickr.com/9/8574/16635664865_9f5e9e2918_z.jpg', 'https://c5.staticflickr.com/3/2929/14195441260_7201745aaa_z.jpg'];
+// myImgFolder.forEach()
+var counter = 0;
+function frame() {
+  var progressB = document.querySelector('progress');
+  counter++;
+  progressB.value = (counter/myImgFolder.length)*100;
+}
+
+myImgFolder.forEach(function(myImage, index) {
+  var img = document.createElement('img');
+  document.body.appendChild(img);
+  img.setAttribute('src', myImage);
+  img.addEventListener('load', frame);
+});
+
+  // loadImmg()
