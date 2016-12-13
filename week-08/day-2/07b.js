7312c70e647c748c56f4db35154b1f4279c6d7fa
Szabo Rihcard <ignocius@gmail.com>
2016-12-13 12:43:18 +0100

    7b

--- week-08/day-2/07b.js
+++ week-08/day-2/07b.js
@@ -4,11 +4,12 @@
 //  - Add a 'load' event to the image and only show the image to the user when the image is loaded
 
 var img = document.createElement('img');
+var hiddenOrNot = function(){
+  img.style.visibility = 'visible';
+};
 img.setAttribute('src', 'https://c7.staticflickr.com/8/7562/15584243094_b268a32192_h.jpg');
 img.style.visibility = 'hidden';
-img.addEventListener('load', function(){
-  img.style.visibility = 'visible';
-})
+img.addEventListener('load', hiddenOrNot);
 
 
 document.body.appendChild(img);
