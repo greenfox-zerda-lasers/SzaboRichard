360336edd3488a49ec6d36e841d362079289a3ac
Szabo Rihcard <ignocius@gmail.com>
2016-12-21 17:37:23 +0100

    bit messy but working

--- week-09/day-1/toDoServer.js
+++ week-09/day-1/toDoServer.js
@@ -6,44 +6,68 @@ var bodyParser = require('body-parser');
 
 var todoApp = express();
 
-var todoData = require('./motherflippindata');
+// var todoData = require('./motherflippindata');
 
-var userId = todoData.length;
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: "'root'",
+  password: 'admin',
+  database: 'todo'
+});
+
+// var userId = todoData.length;
 
 todoApp.use(bodyParser.urlencoded({ extended: false }));
 todoApp.use(bodyParser.json());
 
 todoApp.use('/', express.static('./public'));
 
-todoApp.get('/todos', function getAppData(req, res) {
-  res.send(JSON.stringify(todoData));
+con.connect(function(err){
+  if (err) {
+    console.log('An erro war given while connected, conn failed!');
+    return;
+  }
+  console.log('Connection RDY');
 });
 
-todoApp.get('/todos/:id', function getAppDataId(req, res) {
-  res.send('The listed data by id: ' + JSON.stringify(todoData[parseInt(req.params.id)]));
+
+todoApp.get('/todos', function getAppData(req, res) {
+  con.query('SELECT *\
+            FROM todo;', function(err, rows){
+              if (err) {
+                console.log(err.toString());
+                return;
+              }
+              rows = rows.map(function(el){
+                el.completed = (el.completed) ? true : false;
+                return el;
+              })
+              res.send(JSON.stringify(rows)
+              );
+            })
 });
 
 
 todoApp.post('/todos/', function postAppData(req, res) {
-  var userText = req.body.text;
-  var completed = false;
-  userId++;
-  var returnedStuff =
-    {
-          "completed": completed,
-          "id": userId,
-          "text": req.body.text
-    };
-  todoData.push(returnedStuff);
-  res.send(returnedStuff);
+  con.query('INSERT INTO todo(text, completed) VALUES (\'' + req.body.text + '\', 0)', function(err, rows){
+      if (err) {
+        console.log(err.toString());
+      } else {
+        res.send(rows);
+      }
+   });
 });
 
 todoApp.delete('/todos/:id', function deleteAppData(req, res) {
-  var deletedItem = todoData.filter(function(item) {
-    return item.id != req.params.id;
+  con.query('DELETE FROM todo WHERE id = \'' + req.params.id + '\'', function(err, rows){
+    if (err) {
+      console.log(err.toString());
+    } else {
+      res.send(rows);
+    }
   });
-  todoData = deletedItem;
-  res.send(todoData);
 });
 
 todoApp.listen(3000);
