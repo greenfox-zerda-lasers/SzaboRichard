7a3bc4c3ec140d32c16a04ac7e3e2609c59c6f3c
Szabo Rihcard <ignocius@gmail.com>
2017-01-04 17:21:49 +0100

    endpoints maybe workig

--- week-10/server/index.js
+++ week-10/server/index.js
@@ -1,81 +1,110 @@
 'use strict';
 
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
-var playlists = [
-    [{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
-    { "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" }],
 
-    [{ "id": 33, "title": "Helloo", "artist": "bjdfbje", "duration": 577, "path": "c:/music/helloo.mp3" },
-    { "id": 111, "title": "Brooklyn", "artist": "cats", "duration": 52.12, "path": "c:/music/cats/Brooklyn.mp3" }]
-];
+var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : "'root'",
+      password : 'admin',
+      database : 'foxplayer'
+});
+connection.connect();
+
+// var playlists = [
+//     [{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
+//     { "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" }],
+//
+//     [{ "id": 33, "title": "Helloo", "artist": "bjdfbje", "duration": 577, "path": "c:/music/helloo.mp3" },
+//     { "id": 111, "title": "Brooklyn", "artist": "cats", "duration": 52.12, "path": "c:/music/cats/Brooklyn.mp3" }]
+// ];
 
 app.get('/playlists', function (req, res) {
-  res.json(playlists);
+  connection.query('SELECT * FROM playlists', function(err, rows, fields){
+    if (err) {
+      throw err;
+    } else {
+      res.send(rows);
+    }
+  })
 });
 
-app.post("/playlists", function postPlaylist (req, res) {
-  playlists.push([{"id": 3633}])
-  res.send(playlists)
-})
+// a
+
+app.post("/playlists/", function postPlaylist (req, res) {
+  connection.query({
+  		sql: 'INSERT INTO playlists(playlist, system) VALUES(?, ?)',
+  		values: ['Hello', 0]
+  	}, function(err, rows, fields) {
+  		if (err) throw err;
+      // console.log(rows);
+      // rows = rows.map(rows);
+    	res.send(rows);
+    });
+});
+
 
 
 app.delete("/playlists/:id", function deletePlaylist (req, res) {
-  var result = false
-  playlists.forEach(function(playlist, index) {
-    playlist.forEach(function(track, num) {
-      console.log(track.id)
-      console.log(req.params.id, "string")
-      if (req.params.id == track.id) {
-        playlist.splice(num, 1)
-        result = true
-      }
-    })
-  })
-  if (result == false) {
-    res.sendStatus(404)
-  }
-  res.send(playlists)
-})
+  connection.query('DELETE FROM playlists WHERE id = \'' + req.params.id + '\'', function(err, rows){
+    if (err) {
+      console.log(err.toString());
+    }
+    res.send(rows);
+  });
 
-
-app.get('/playlist/tracks', function (req, res) { //path!!
-  res.json(playlists[0][0].title + " " + playlists[0][0].artist)
 });
 
 
-app.get("/playlist-tracks/:playlist_id", function deletePlaylist (req, res) {
-  var num = req.params.playlist_id
-  console.log(num)
-  var result = false
-  playlists.forEach(function(playlist, index) {
-    if (index == num) {
-      result = true
-    }
-  });
-  if (result == false) {
-    res.sendStatus(404)
-  }
-  res.send(playlists[num])
-})
+// app.get('/playlist-tracks/', function (req, res) {
+//   connection.query('SELECT * FROM tracks', function(err, rows){
+//     if (err) {
+//       throw err;
+//     }
+//     res.send(rows);
+//   });
+//   // res.json(playlists[0][0].title + " " + playlists[0][0].artist)
+// });
+//
+//
+// app.get("/playlist-tracks/:track_id", function getPlaylist (req, res) {
+//   connection.query('SELECT * FROM tracks WHERE playlist_id = \'' + req.params.id + '\'', function(err, rows){
+//     if (err) {
+//       throw err;
+//     }
+//     res.send(rows);
+//   });
+// });
+//
+// app.post("/playlist-tracks/:playlist_id", function postPlaylist (req, res) {
+//   connection.query({
+//     sql : 'INSERT INTO tracks(path, playlist_id) VALUES(?,?)',
+//     values : ['test', req.params.playlist_id]
+//   }, function(err, rows){
+//     if (err) {
+//       throw err;
+//     }
+//     res.send(rows);
+//   });
+// });
 
-
-app.post("/playlist-track/:playlist_id", function postPlaylist (req, res) {
-  var num = req.params.playlist_id
-  var newTrack = {"name": "new_track"}
-  var result = false
-  playlists.forEach(function(playlist, index) {
-    if (index == num) {
-      result = true
-      playlists[num].push(newTrack)
+app.get("/playlist-tracks/:playlist_id/:track_id", function deletePlaylist (req, res) {
+  connection.query({
+    sql : 'DELETE FROM tracks WHERE playlist_id = ? AND id = ?' ,
+    values : [req.params.playlist_id, req.params.track_id]
+    }, function(err, rows){
+    if (err) {
+      throw err;
     }
+    res.send(rows);
   });
-  if (result == false) {
-    res.sendStatus(404)
-  }
-  res.send(playlists[num])
-})
+});
 
 
 
 module.exports = app;
+
+app.listen(3000, function(){
+	console.log('SERVER IS UP AND RUNNIN on port: 3000')
+});
